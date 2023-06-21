79388906aad5e2c79ea3a22b4433054514597368
yala0 <welcome.yanlafitte@gmail.com>
2023-06-21 20:36:40 +0200

    add lazy load on pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,79 @@ import {
    createRoutesFromElements,
    Route,
 } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
 import RootLayout from './pages/RootLayout';
-import ErrorPage from './pages/ErrorPage';
-import Agency from './pages/Agency';
-import Contact from './pages/Contact';
-import Whoarewe from './pages/Whoarewe';
-import Work from './pages/Work';
-import Team from './pages/Team';
-import Localisation from './pages/Localisation';
+import Loader from './components/Loader';
+
+const ErrorPage = lazy(() => import('./pages/ErrorPage'));
+const Agency = lazy(() => import('./pages/Agency'));
+const Whoarewe = lazy(() => import('./pages/Whoarewe'));
+const Work = lazy(() => import('./pages/Work'));
+const Team = lazy(() => import('./pages/Team'));
+const Localisation = lazy(() => import('./pages/Localisation'));
+const Contact = lazy(() => import('./pages/Contact'));
+
+const renderLoader = () => <Loader />;
 
 const router = createBrowserRouter(
    createRoutesFromElements(
-      <Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
-         <Route index element={<Agency />} />
-         <Route path='/whoarewe' element={<Whoarewe />} />
-         <Route path='/work' element={<Work />} />
-         <Route path='/team' element={<Team />} />
-         <Route path='/localisation' element={<Localisation />} />
-         <Route path='/contact' element={<Contact />} />
+      <Route
+         path='/'
+         element={<RootLayout />}
+         errorElement={
+            <Suspense>
+               <ErrorPage />
+            </Suspense>
+         }
+      >
+         <Route
+            index
+            element={
+               <Suspense fallback={renderLoader}>
+                  <Agency />
+               </Suspense>
+            }
+         />
+         <Route
+            path='/whoarewe'
+            element={
+               <Suspense fallback={renderLoader}>
+                  <Whoarewe />
+               </Suspense>
+            }
+         />
+         <Route
+            path='/work'
+            element={
+               <Suspense fallback={renderLoader}>
+                  <Work />
+               </Suspense>
+            }
+         />
+         <Route
+            path='/team'
+            element={
+               <Suspense fallback={renderLoader}>
+                  <Team />
+               </Suspense>
+            }
+         />
+         <Route
+            path='/localisation'
+            element={
+               <Suspense fallback={renderLoader}>
+                  <Localisation />
+               </Suspense>
+            }
+         />
+         <Route
+            path='/contact'
+            element={
+               <Suspense fallback={renderLoader}>
+                  <Contact />
+               </Suspense>
+            }
+         />
       </Route>
    )
 );
